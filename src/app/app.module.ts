df11080e195848ac8ce9ea61e1fc095d7d8abeca
Herre <jaherrerias@gmail.com>
2017-01-02 01:50:25 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+//FormsModule added by ng-client gives us template driven directives such as NgModel and NgForm
+//ReactiveFormsModule added manually gives us directives such as formControl and NgFormGroup and several more
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ShippingInformationComponent } from './shipping-information/shipping-information.component';
+import { ProductPictureComponent } from './product-picture/product-picture.component';
+import { ProductSizeComponent } from './product-size/product-size.component';
+import { ProductCompositionComponent } from './product-composition/product-composition.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShippingInformationComponent,
+    ProductPictureComponent,
+    ProductSizeComponent,
+    ProductCompositionComponent,
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, //Necessary for working with Forms and added by ng-client
+    ReactiveFormsModule, //Necessary for working with Forms and added manually
     HttpModule
   ],
   providers: [],
